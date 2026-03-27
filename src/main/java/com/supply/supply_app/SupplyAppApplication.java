f06a189d0abcd6b8436332fe0ddadf717b84fde5
aniltekdemir <anil@ximmio.com>
2026-03-27 06:56:10 +0300

    feat: initial Spring Boot project setup

---
+++ src/main/java/com/supply/supply_app/SupplyAppApplication.java
@@ -0,0 +1,13 @@
+package com.supply.supply_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supply/supply_app/SupplyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supply.supply_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
